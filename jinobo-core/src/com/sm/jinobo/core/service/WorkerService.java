3983ee4c5baf450879372e052e252d3be630ca4e
shivdatta.marathe <shivdatta.marathe@gmail.com>
2013-09-18 00:48:51 +0530

    Submitting the core and POP3 and IMAP4 implementation

---
+++ jinobo-core/src/com/sm/jinobo/core/service/WorkerService.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.sm.jinobo.core.service;
+
+/**
+ * Worker service which needs to be implemented by the types of workers.
+ * @author smarathe
+ *
+ */
+public interface WorkerService {
+
+}
